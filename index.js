f048a9eab690b5560b7b3c1f20f392015d6f5831
Jorge Gallego <gallegojorge908@gmail.com>
2021-08-29 11:06:12 -0500

    Fixed issues with setInterval

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ document.querySelector("i.bi-chevron-left").addEventListener("click", () => {
     let backgroundImage = document.querySelector(".carousel").style.backgroundImage.split("-")[1][0];
     backgroundImage==1 ? backgroundImage=6 : backgroundImage--;
     document.querySelector(".carousel").style.backgroundImage = `url(./images/background-${backgroundImage}.jpg`;
+    clearInterval(timer);
+    timer = setInterval(bg, 10000);
 });
 
 document.querySelector("i.bi-chevron-right").addEventListener("click", () => {
     let backgroundImage = document.querySelector(".carousel").style.backgroundImage.split("-")[1][0];
     backgroundImage==6 ? backgroundImage=1 : backgroundImage++;
     document.querySelector(".carousel").style.backgroundImage = `url(./images/background-${backgroundImage}.jpg`;
+    clearInterval(timer);
+    timer = setInterval(bg, 10000);
 });
 
-setInterval(() => {
+const bg = () => {
     document.querySelector("i.bi-chevron-right").click();
-}, 10000);
+}
+
+let timer = setInterval(bg, 10000);
